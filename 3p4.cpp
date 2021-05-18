25d7e91acd4a7c50a92d9748099cf273f1e4661f
shraditya <56548373+shraditya@users.noreply.github.com>
2021-05-18 14:04:27 +0530

    Create 3p4.cpp

---
+++ 3p4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    list<int> arr;
+    
+    arr.push_back(123);
+    cout << arr.front();
+
+    return 0;
+}
